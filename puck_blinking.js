40b30166376e9848b627c5b7d2187cbe9171b06a
Manuel Keglevic <keglevic@cvl.tuwien.ac.at>
2023-02-07 14:32:48 +0100

    Add multiple states to blinking script

--- puck_blinking.js
+++ puck_blinking.js
@@ -1,27 +1,36 @@
-let led = false;
+let state = 0;// 0 - off, 1 - on, 2 - blinking
+const n_states = 3;
 
+let led = false;
 let toggle = () => {
     led = !led;
     digitalWrite(D1, led);
 };
 
-let start = () => {
+let blinking = () => {
+    toggle();
     setInterval(function () {
         toggle();
     }, 500);
 };
 
-let stop = () => {
-    clearInterval();
+let on = () => {
+    digitalWrite(D1, true);
 };
 
-let blinking = false;
+let off = () => {
+    clearInterval();
+    digitalWrite(D1, false);
+};
+
 setWatch(function () {
-    if (!blinking) {
-        start();
-    } else {
-        stop();
+    state = (state + 1) % n_states;
+
+    switch (state) {
+        case 0: off(); break;
+        case 1: on(); break;
+        case 2: blinking(); break;
+        default: off(); break;
     }
-    blinking = !blinking;
 }, BTN, { edge: "rising", debounce: 50, repeat: true });
 
